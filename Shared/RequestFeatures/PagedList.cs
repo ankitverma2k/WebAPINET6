8c78554e3674dc69f2a5f6ad39f0a54c1aa5f37c
ANKIT <ankitverma2k@gmail.com>
2025-04-14 11:20:40 +0530

    Add pagination support with MetaData and PagedList classes
    Introduce `MetaData` and `PagedList<T>` classes in the
    `Shared.RequestFeatures` namespace to facilitate pagination.
    `MetaData` holds pagination details, while `PagedList<T>`
    extends `List<T>` and provides methods for creating paged
    lists from collections.

---
+++ Shared/RequestFeatures/PagedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.RequestFeatures
+{
+    public class PagedList<T> : List<T>
+    {
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, int count, int PageNumber, int PageSize)
+        {
+            MetaData = new MetaData
+            {
+                TotalCount = count,
+                PageSize = PageSize,
+                CurrentPage = PageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)PageSize)
+
+            };
+            AddRange(items);
+        }
+        public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count(); var items = source
+                .Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList(); return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+
+    }
+}
