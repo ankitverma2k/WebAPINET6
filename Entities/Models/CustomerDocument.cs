ee44bd2fdc0b8effa1ae233f3f52a41acc6a0c66
ANKIT <ankitverma2k@gmail.com>
2025-09-18 11:49:06 +0530

    Add customer document management functionality
    Introduces the ICustomerDocumentRepository interface for managing customer documents, along with the CustomerDocument class representing document properties. Implements the CustomerDocumentRepository class with method stubs for CRUD operations.

---
+++ Entities/Models/CustomerDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class CustomerDocument
+    {
+        public Guid Id { get; set; }
+        public string ? FileName { get; set; }
+        public byte[] ? Content { get; set; }
+
+    }
+}
